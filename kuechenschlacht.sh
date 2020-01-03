fb477cf9c8f3dcf08bb59df2dbfa3a7e511559b3
jwbensley <jwbensley@gmail.com>
2020-01-03 14:17:32 +0000

    Added additional quality settings

--- kuechenschlacht.sh
+++ kuechenschlacht.sh
@@ -41,23 +41,30 @@ if [[ " ${args[@]} " =~ "-h" ]]
 then
     echo "usage: $0 [DD] [MM] [YY] [QUALITY]"
     echo ""
-    echo "Defaults to todays date and maximum quality (3296000):"
+    echo "Defaults to todays date and maximum quality (3328000):"
     echo ""
-    echo "Default: $0 `date +'%d'` `date +'%m'` `date +'%y'` 3296000"
+    echo "Default: $0 `date +'%d'` `date +'%m'` `date +'%y'` 3328000"
     echo ""
     echo "Quality options:"
     echo "476000  : BANDWIDTH=388000,RESOLUTION=480x272,CODECS='avc1.77.30, mp4a.40.2'"
+    echo "508000  : BANDWIDTH=424000,RESOLUTION=480x270,CODECS='avc1.77.30, mp4a.40.2'"
     echo "776000  : BANDWIDTH=623000,RESOLUTION=640x360,CODECS='avc1.77.30, mp4a.40.2'"
+    echo "808000  : BANDWIDTH=665000,RESOLUTION=640x360,CODECS='avc1.77.30, mp4a.40.2'"
     echo "1496000 : BANDWIDTH=1193000,RESOLUTION=852x480,CODECS='avc1.77.30, mp4a.40.2'"
+    echo "1628000 : BANDWIDTH=1314000,RESOLUTION=960x540,CODECS='avc1.77.30, mp4a.40.2'"
     echo "2296000 : BANDWIDTH=1830000,RESOLUTION=1024x576,CODECS='avc1.77.30, mp4a.40.2'"
     echo "3296000 : BANDWIDTH=2583000,RESOLUTION=1280x720,CODECS='avc1.640028, mp4a.40.2'"
+    echo "3328000 : BANDWIDTH=2652000,RESOLUTION=1280x720,CODECS='avc1.640028, mp4a.40.2'"
+    echo ""
+    echo "Note: Not all episodes are available every qualities"
+    echo ""
     exit 1
 fi
 
 day_2="`date +'%d'`"
 month_2="`date +'%m'`"
 year_2="`date +'%y'`"
-quality="3296000"
+quality="3328000"
 
 if [ ! -z "$1" ]
 then
