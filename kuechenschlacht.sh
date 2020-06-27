87d76392c75003c91a892acbf1f1f36a42a4f819
jwbensley <jwbensley@gmail.com>
2020-06-27 19:35:56 +0100

    Updated to support newer episode which are a single file, not chunked

--- kuechenschlacht.sh
+++ kuechenschlacht.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+# $1 == ep_url
+# $2 == use_wget
+function dl_single() {
+
+    if [ $2 -eq 1 ]
+    then
+        echo -e "\nUsing wget:"
+        # wget args:
+        # -c, --continue       resume getting a partially-downloaded file
+        # Not all wget versions (i.e. Ubuntu) support --show-progress
+        # wget exit status 8 means "Server issued an error response", the server
+        # sends a 405 message when we have a file clobber (we already have this
+        # episode) so it's safe to ignore.
+        wget -c "$1"
+        ret="$?"
+        if [ $ret -ne 0 ] && [ $ret -ne 8 ]; then return 1; fi
+    else
+        echo -e "\nUsing curl:"
+        # -C -                 resume
+        curl -C - -O "$1"
+        if [ $? -ne 0 ]; then return 1; fi
+    fi
+
+    return 0
+
+}
+
 
 # Ensure required bins are present
 which wget > /dev/null
@@ -20,20 +47,6 @@ else
     fi
 fi
 
-which ffmpeg > /dev/null
-if [ $? -ne 0 ]
-then
-    echo "ffmpeg is required but missing. Exiting."
-    exit 1
-fi
-
-if [ $(ffmpeg 2>&1 | grep -c "Libav") -gt 0 ]
-then
-    echo "It appears that avconv is installed instead of ffmpeg."
-    echo "avconv is not support (yet). Exiting."
-    exit 1
-fi
-
 
 # Parse CLI args and display help test:
 args=("$@")
@@ -41,11 +54,16 @@ if [[ " ${args[@]} " =~ "-h" ]]
 then
     echo "usage: $0 [DD] [MM] [YY] [QUALITY]"
     echo ""
-    echo "Defaults to todays date and maximum quality (3328000):"
+    echo "Defaults to todays date and maximum quality (2128):"
     echo ""
-    echo "Default: $0 `date +'%d'` `date +'%m'` `date +'%y'` 3328000"
+    echo "Default: $0 `date +'%d'` `date +'%m'` `date +'%y'` 2128"
     echo ""
-    echo "Quality options:"
+    echo "Quality options (for whole episodes):"
+    echo "368     : NAME=Hoch,BANDWIDTH=368kbps,RESOLUTION=480x270,CODEC=Google/On2 VP9"
+    echo "1128    : NAME=Sehr Hoch,BANDWIDTH=1128Kbps,RESOLUTION=960x540,CODEC=Google/On2 VP9"
+    echo "2128    : Name=HD,BANDWIDTH=2129Kbps,RESOLUTION=1280x720,CODEC=Google/On2 VP9"
+    echo ""
+    echo "Quality options (for chunked episodes):"
     echo "476000  : BANDWIDTH=388000,RESOLUTION=480x272,CODECS='avc1.77.30, mp4a.40.2'"
     echo "508000  : BANDWIDTH=424000,RESOLUTION=480x270,CODECS='avc1.77.30, mp4a.40.2'"
     echo "776000  : BANDWIDTH=623000,RESOLUTION=640x360,CODECS='avc1.77.30, mp4a.40.2'"
@@ -61,10 +79,12 @@ then
     exit 1
 fi
 
+
+# Set default args
 day_2="`date +'%d'`"
 month_2="`date +'%m'`"
 year_2="`date +'%y'`"
-quality="3328000"
+quality="2128"
 
 if [ ! -z "$1" ]
 then
@@ -86,15 +106,60 @@ then
     quality="$4"
 fi
 
-#date_6="`date +'%y%m%d'`"
 date_6="$year_2"
 date_6+="$month_2"
 date_6+="$day_2"
 d="./$date_6/"
 mkdir -p "$d"
 cd "$d"
+echo "Downloading Die Küchenschlacht episode for $date_6:"
 
 
+# Older episodes are available in chunks,
+# newer episodes are available as a single video file.
+# Try to download as a single file...
+if [ "$quality" -eq "368" ]
+then
+    qual="368k_p16v15"
+elif [ "$quality" -eq "1128" ]
+then
+    qual="1128k_p17v15"
+else
+    qual="2128k_p18v15"
+fi
+ep_url="https://nrodlzdf-a.akamaihd.net/none/zdf/"
+ep_url+="$year_2/$month_2/"
+ep_url+="$year_2$month_2"
+ep_url+="$day_2"
+ep_url+="_sendung_1415_dku/1/"
+ep_url+="$year_2$month_2"
+ep_url+="$day_2"
+ep_url+="_sendung_1415_dku_$qual.webm"
+dl_single $ep_url $use_wget
+if [ $? -eq 0 ]
+then
+    sync
+    echo "Finished"
+    exit 0
+fi
+
+
+# If the single file download failed, try the chunked file method.
+# If a chunked version of an episode is being downloaded, ffmpeg is required
+# to concatenate the chunks together:
+which ffmpeg > /dev/null
+if [ $? -ne 0 ]
+then
+    echo "ffmpeg is required but missing. Exiting."
+    exit 1
+fi
+
+if [ $(ffmpeg 2>&1 | grep -c "Libav") -gt 0 ]
+then
+    echo "It appears that avconv is installed instead of ffmpeg."
+    echo "avconv is not support (yet). Exiting."
+    exit 1
+fi
 
 # Download the daily playlist file which contains URLs to all the individual
 # segments which make the episode:
@@ -107,7 +172,7 @@ playlist_url+="_sendung_dku.smil/index_$quality"
 playlist_url+="_av.m3u8"
 playlist_filename=`basename "$playlist_url"`
 
-echo "Downloading Die Küchenschlacht episode for $date_6:"
+
 # wget will return a non-zero exist status if the playlist URL is invalid,
 # curl will return 0. In the case of curl, an error message is returned by the
 # remote server, check for the presence of the error message in the output file:
